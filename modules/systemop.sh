9b846f8b48f7072f8e171ec146e1ebb8588c5f88
Davide Polonio <poloniodavide@gmail.com>
2018-07-18 11:19:16 +0200

    Fix typo

--- modules/systemop.sh
+++ modules/systemop.sh
@@ -41,16 +41,16 @@ function install_ubuntu () {
 function install () {
     local package_list="$1"
     
-    if [ -z "MB_DISTRO" ]
+    if [ -z "$MB_DISTRO" ]
     then
         set_distro "$(detect_distro)"
     fi
 
-    case $MB_DISTRO in
-        ubuntu)
+    case "$MB_DISTRO" in
+        "ubuntu")
             install_ubuntu "$package_list"
             ;;
-        centos)
+        "centos")
             install_centos "$package_list"
             ;;
         *)
